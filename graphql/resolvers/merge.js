a4813c9549f13849be0c17688f354da474b33883
Juan Pablo Perez <Juan.Pablo.Perez@retailinmotion.com>
2019-02-05 17:29:54 +0000

    20. Using Dataloader

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -1,6 +1,52 @@
+const DataLoader = require("dataloader");
+
 const Event = require("../../models/event");
 const User = require("../../models/user");
 const { dateToString } = require("../../helpers/date");
+
+const eventLoader = new DataLoader(eventIds => {
+  return events(eventIds);
+});
+
+const userLoader = new DataLoader(userIds => {
+  return User.find({ _id: { $in: userIds } });
+});
+
+const events = async eventIds => {
+  try {
+    const events = await Event.find({ _id: { $in: eventIds } });
+    return events.map(event => {
+      return transformEvent(event);
+    });
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+const singleEvent = async eventId => {
+  try {
+    const event = await eventLoader.load(eventId.toString());
+    return event;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+const user = async userId => {
+  try {
+    const user = await userLoader.load(userId.toString());
+    return {
+      ...user._doc,
+      _id: user.id,
+      createdEvents: eventLoader.loadMany.bind(this, user._doc.createdEvents)
+    };
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
 
 const transformEvent = event => {
   if (event) {
@@ -26,42 +72,6 @@ const transformBooking = booking => {
     };
   }
   return null;
-};
-
-const events = async eventIds => {
-  try {
-    const events = await Event.find({ _id: { $in: eventIds } });
-    return events.map(event => {
-      return transformEvent(event);
-    });
-  } catch (err) {
-    console.log(err);
-    throw err;
-  }
-};
-
-const singleEvent = async eventId => {
-  try {
-    const event = await Event.findById(eventId);
-    return transformEvent(event);
-  } catch (err) {
-    console.log(err);
-    throw err;
-  }
-};
-
-const user = async userId => {
-  try {
-    const user = await User.findById(userId);
-    return {
-      ...user._doc,
-      _id: user.id,
-      createdEvents: events.bind(this, user._doc.createdEvents)
-    };
-  } catch (err) {
-    console.log(err);
-    throw err;
-  }
 };
 
 exports.transformEvent = transformEvent;
